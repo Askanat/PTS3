a8ef57e0079aa51499743fd771db647ffb25eaba
bastien <bastien.chanez@gmail.com>
2016-09-29 17:37:30 +0200

    complement des controles

--- src/controleur/ControlGroup.java
+++ src/controleur/ControlGroup.java
@@ -9,18 +9,18 @@ import vue.Fenetre;
 
 public class ControlGroup {
 
-    Jeu jeu = new Jeu();
-    Fenetre fenetre;
+    public Jeu jeu = new Jeu();
+    public Fenetre fenetre;
 
     public ControlGroup(Jeu jeu) {
         this.jeu = jeu;
-        fenetre = new Fenetre(jeu);
+        fenetre = new Fenetre(this.jeu);
 
-        ControlFenetreCharger controlFenetreCharger = new ControlFenetreCharger(jeu, fenetre);
-        ControlFenetreCredits controlFenetreCredits = new ControlFenetreCredits(jeu, fenetre);
-        ControlFenetreDepart controlFenetreDepart = new ControlFenetreDepart(jeu, fenetre);
-        ControlFenetreDonjon controlFenetreDonjon = new ControlFenetreDonjon(jeu, fenetre);
-        ControlFenetreNouvellePartie controlFenetreNouvellePartie = new ControlFenetreNouvellePartie(jeu, fenetre);
-        ControlMenuPrincipal controlMenuPrincipal = new ControlMenuPrincipal(jeu, fenetre);
+        ControlFenetreCharger controlFenetreCharger = new ControlFenetreCharger(this.jeu, fenetre);
+        ControlFenetreCredits controlFenetreCredits = new ControlFenetreCredits(this.jeu, fenetre);
+        ControlFenetreDepart controlFenetreDepart = new ControlFenetreDepart(this.jeu, fenetre);
+        ControlFenetreDonjon controlFenetreDonjon = new ControlFenetreDonjon(this.jeu, fenetre);
+        ControlFenetreNouvellePartie controlFenetreNouvellePartie = new ControlFenetreNouvellePartie(this.jeu, fenetre);
+        ControlMenuPrincipal controlMenuPrincipal = new ControlMenuPrincipal(this.jeu, fenetre);
     }
 }
