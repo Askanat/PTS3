69bf4fd9a8efa0f9b6736621d61d2743f82a3b51
FlorianVaissiere <florian.vaissiere@orange.fr>
2016-10-05 09:31:56 +0200

    Debut Barre Menu

---
+++ src/vue/BarreMenu.java
@@ -0,0 +1,11 @@
+package vue;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by Florian Vaissiere on 03/10/2016.
+ */
+public class BarreMenu {
+    private JMenuBar menuBar = new JMenuBar();
+}
