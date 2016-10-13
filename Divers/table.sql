2a89e4e83af3a5556dc09dc32db9e54e19b6e156
bastien <bastien.chanez@gmail.com>
2016-10-13 22:44:40 +0200

    BDD

--- Divers/table.sql
+++ Divers/table.sql
@@ -30,10 +30,10 @@ CREATE TABLE monstre (
 	PRIMARY KEY(id)
 );
 
-INSERT INTO hero VALUES (NULL, 'hero1', 1, 0, 100, 4,0, 0, 0, 0, 100, 100,"image/texture_hero.png");
-INSERT INTO hero VALUES (NULL, 'hero2', 1, 0, 100, 4,0, 0, 0, 0, 100, 100,"image/texture_hero.png");
-INSERT INTO hero VALUES (NULL, 'hero3', 1, 0, 100, 4,0, 0, 0, 0, 100, 100,"image/texture_hero.png");
+INSERT INTO hero VALUES (NULL, 'hero1', 1, 0, 100, 4,0, 0, 0, 0, 100, 100,"images/texture_hero.png");
+INSERT INTO hero VALUES (NULL, 'hero2', 1, 0, 100, 4,0, 0, 0, 0, 100, 100,"images/texture_hero.png");
+INSERT INTO hero VALUES (NULL, 'hero3', 1, 0, 100, 4,0, 0, 0, 0, 100, 100,"images/texture_hero.png");
 
-INSERT INTO monstre VALUES (NULL, 'monstre1', 1, 0, 0, 0,0, 20,"image/texture_monstre.png");
-INSERT INTO monstre VALUES (NULL, 'monstre2', 1, 0, 0, 0,0, 20,"image/texture_monstre.png");
-INSERT INTO monstre VALUES (NULL, 'monstre3', 1, 0, 0, 0,0, 20,"image/texture_monstre.png");
+INSERT INTO monstre VALUES (NULL, 'monstre1', 1, 0, 0, 0,0, 20,"images/texture_monstre.png");
+INSERT INTO monstre VALUES (NULL, 'monstre2', 1, 0, 0, 0,0, 20,"images/texture_monstre.png");
+INSERT INTO monstre VALUES (NULL, 'monstre3', 1, 0, 0, 0,0, 20,"images/texture_monstre.png");
