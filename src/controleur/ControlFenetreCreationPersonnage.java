793aca49bd56d414e3569713ee5cb0783729ef1d
leo <leo.petit90@gmail.com>
2016-11-27 22:13:48 +0100

    Nouvelle partie fonctionelle :)

--- src/controleur/ControlFenetreCreationPersonnage.java
+++ src/controleur/ControlFenetreCreationPersonnage.java
@@ -24,11 +24,36 @@ public class ControlFenetreCreationPersonnage extends Control implements ActionL
                 jeu.createHeroBDD(nomPerso, idHero);
                 jeu.setHero(idHero);
                 fenetre.panelFenetreDepart.dessineHero(idHero);
+
+                spawnHero(idHero);
+                fenetre.barreMenu.test(); // ajout du composant vie dans la bare menu
+                spawnMonstre(1, Fenetre.adapterResolutionEnX(300), Fenetre.adapterResolutionEnY(250)); // a enlever d'ici
+                spawnMonstre(2, Fenetre.adapterResolutionEnX(600), Fenetre.adapterResolutionEnY(700)); // a enlever d'ici
+                spawnMonstre(3, Fenetre.adapterResolutionEnX(900), Fenetre.adapterResolutionEnY(900)); // a enlever d'ici
+
+                Control.enPartie = true;
+                fenetre.barreMenu.setVisible(true);
+                fenetre.setContentPane(fenetre.panelFenetreDepart);
+                changerVue();
+                Control.enPartie = true;
+                fenetre.barreMenu.setVisible(true);
+                fenetre.setContentPane(fenetre.panelFenetreDepart);
+                changerVue();
                 break;
             case "Retour":
                 fenetre.setContentPane(fenetre.panelMenuPrincipal);
                 changerVue();
                 break;
         }
+    }
+
+    private void spawnHero(int i) {
+        jeu.setHero(i);
+        fenetre.panelFenetreDepart.dessineHero(i);
+    }
+
+    private void spawnMonstre(int i, int positionX, int positionY) { // a enlever d'ici
+        jeu.setMonstre(i, positionX, positionY);
+        fenetre.panelFenetreDepart.dessineMonstre(i);
     }
 }
