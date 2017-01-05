ef0b02769b809942c43be682ae0a8213939d7484
FlorianVaissiere <florian.vaissiere@orange.fr>
2017-01-05 14:53:19 +0100

    Ajout Commentaire et réorganisation des fonctions HERO

--- src/model/Hero.java
+++ src/model/Hero.java
@@ -52,7 +52,6 @@ public class Hero extends Personnage {
         this.forcePerso = forcePerso;
         this.constiPerso = constiPerso;
         this.intelPerso = intelPerso;
-        this.forcePerso = forcePerso;
         this.resiPerso = resiPerso;
 
         this.or = or;
@@ -80,56 +79,100 @@ public class Hero extends Personnage {
         );
     }
 
-    public boolean checkNiveauSup() {
-        return getExperience() >= getExperienceMax();
-    }
-
-    public void upNiveau() {
-        if (getExperience() == getExperienceMax()){
-            setNiveau(getNiveau() + 1);
-            update_ExperienceMax();
-            setPointCaractéristique();
-            setPointCompetence();
+    //Verification et Augmentation de Niveau avec ajout de Points de Compétences et Caractéristiques et augmentation du seuil d'expérience à atteindre
+        public boolean checkNiveauSup() {
+            return getExperience() >= getExperienceMax();
         }
-    }
 
-    public void setPointCaractéristique(){
-        pointCaracteristique = getPointCaracteristique() + 10;
-    }
+        public void upNiveau() {
+            if (checkNiveauSup()){
+                setNiveau(getNiveau() + 1);
+                update_ExperienceMax();
+                setPointCaractéristique();
+                setPointCompetence();
+            }
+        }
 
-    public int getPointCaracteristique() {
-        return pointCaracteristique;
-    }
+    //Modification Caractéristiques
+        //Force
+            public void setForcePerso(double forcePerso){
+                this.forcePerso = forcePerso;
+            }
 
-    public void setPointCompetence(){
-        pointCompetence = getPointCompetence() + 1;
-    }
+            public double getForcePerso() {
+                return forcePerso;
+            }
+        //Intelligence
+            public void setIntelPerso(double intelPerso) {
+                this.intelPerso = intelPerso;
+            }
 
-    public int getPointCompetence() {
-        return pointCompetence;
-    }
+            public double getIntelPerso() {
+                return intelPerso;
+            }
+        //Constitution
+            public void setConstiPerso(double constiPerso) {
+                this.constiPerso = constiPerso;
+            }
 
-    public void recevoirExperience(Monstre m) {
-        setExperience(getExperience() + m.donneExperience());
-    }
+            public double getConstiPerso() {
+                return constiPerso;
+            }
+        //Résistance
+            public void setResiPerso(double resiPerso) {
+                this.resiPerso = resiPerso;
+            }
 
-    public void update_ExperienceMax() {
-        setExperienceMax(2 * (-700 + (500 * (getNiveau() + 1)) - (80 * pow((getNiveau() + 1), 2)) + (10 * pow((getNiveau() + 1), 3))));
-    }
+            public double getResiPerso() {
+                return resiPerso;
+            }
 
-    public double getExperience() {
-        return experience;
-    }
+    //Gestion de l'ajout de Point de Caractéristique et le Return
+        public void setPointCaractéristique(){
+            pointCaracteristique = getPointCaracteristique() + 10;
+        }
 
-    public void setExperience(double experience) {
-        this.experience = experience;
-    }
+        public int getPointCaracteristique() {
+            return pointCaracteristique;
+        }
 
-    public void setExperienceMax(double experienceMax) {
-        this.experienceMax = experienceMax;
-    }
+    //Gestion de l'ajout de Point de Compétence et le Return
+        public void setPointCompetence(){
+            pointCompetence = getPointCompetence() + 1;
+        }
 
-    public double getExperienceMax() {
-        return experienceMax;
-    }
+        public int getPointCompetence() {
+            return pointCompetence;
+        }
+
+    //Ajout et update de l'Expérience
+        //Ajout d'EXP
+            //EXP ajouté au héros
+                public void recevoirExperience(Monstre m) {
+                    setExperience(getExperience() + m.donneExperience());
+                }
+
+            //EXP maximum augmenté
+                public void update_ExperienceMax() {
+                    setExperienceMax(2 * (-700 + (500 * (getNiveau() + 1)) - (80 * pow((getNiveau() + 1), 2)) + (10 * pow((getNiveau() + 1), 3))));
+                }
+
+        //Affichage EXP
+            //EXP progression
+                public void setExperience(double experience) {
+                    this.experience = experience;
+                }
+
+                public double getExperience() {
+            return experience;
+        }
+
+            //EXP MAX
+                public void setExperienceMax(double experienceMax) {
+                    this.experienceMax = experienceMax;
+                }
+
+                public double getExperienceMax() {
+            return experienceMax;
+        }
 }
