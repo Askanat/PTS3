7a5b60568f18ee58cc91f5fb3ea0a9c41f81fc27
schliermelvin <miliciiop.77>
2017-01-13 01:38:11 +0100

    fond jeux

--- src/vue/FenetreCreationPersonnage.java
+++ src/vue/FenetreCreationPersonnage.java
@@ -52,7 +52,6 @@ public class FenetreCreationPersonnage extends JPanel {
     public int choixCouleurPilosite;
     public int choixPilosite;
 
-
     public FenetreCreationPersonnage(Jeu jeu) {
 
         this.jeu = jeu;
