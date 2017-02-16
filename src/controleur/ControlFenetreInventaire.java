14accc6f69d2bfd4d8762330480e5626a47c5324
FlorianVaissiere <florian.vaissiere@orange.fr>
2017-02-16 10:40:37 +0100

    Début De l'inventaire

--- src/controleur/ControlFenetreInventaire.java
+++ src/controleur/ControlFenetreInventaire.java
@@ -17,48 +17,136 @@ public class ControlFenetreInventaire extends Control implements ActionListener 
     }
 
     public void actionPerformed(ActionEvent e) {
-        String nombre="";
+        String nombre1="";
+        String nombre2="";
 
         //Système de récupération des boutons d'inventaires différents
         if(e.getActionCommand().contains("Inventaire")){
 
             for (int i=10;i < e.getActionCommand().length();i++){
-                nombre+=e.getActionCommand().charAt(i);
+                nombre1+=e.getActionCommand().charAt(i);
             }
 
-            System.out.println("Bouton : " + Integer.parseInt(nombre));
+            if (e.getActionCommand().contains("Inventaire")){
+                for (int j=10;j < e.getActionCommand().length();j++){
+                    nombre2+=e.getActionCommand().charAt(j);
+                }
+
+            }
+
+            if (e.getActionCommand().contains("Casque")){
+
+            }
+
+            if (e.getActionCommand().contains("Plastron")){
+
+            }
+
+            if (e.getActionCommand().contains("Gant")){
+
+            }
+
+            if (e.getActionCommand().contains("Bague")){
+
+            }
+
+            if (e.getActionCommand().contains("Pantalon")){
+
+            }
+
+            if (e.getActionCommand().contains("Botte")){
+
+            }
+
+            if (e.getActionCommand().contains("Bouclier")){
+
+            }
+
+            if (e.getActionCommand().contains("Arme")){
+
+            }
+
+            System.out.println("Bouton : " + Integer.parseInt(nombre1));
         }
 
         switch (e.getActionCommand()) {
             case "Casque":
+                if(e.getActionCommand().contains("Inventaire")) {
+
+                    for (int i = 10; i < e.getActionCommand().length(); i++) {
+                        nombre1 += e.getActionCommand().charAt(i);
+                    }
+                }
 
                 break;
 
             case "Plastron":
+                if(e.getActionCommand().contains("Inventaire")) {
+
+                    for (int i = 10; i < e.getActionCommand().length(); i++) {
+                        nombre1 += e.getActionCommand().charAt(i);
+                    }
+                }
 
                 break;
 
             case "Gant":
+                if(e.getActionCommand().contains("Inventaire")) {
+
+                    for (int i = 10; i < e.getActionCommand().length(); i++) {
+                        nombre1 += e.getActionCommand().charAt(i);
+                    }
+                }
 
                 break;
 
             case "Bague":
+                if(e.getActionCommand().contains("Inventaire")) {
+
+                    for (int i = 10; i < e.getActionCommand().length(); i++) {
+                        nombre1 += e.getActionCommand().charAt(i);
+                    }
+                }
 
                 break;
 
             case "Pantalon":
+                if(e.getActionCommand().contains("Inventaire")) {
+
+                    for (int i = 10; i < e.getActionCommand().length(); i++) {
+                        nombre1 += e.getActionCommand().charAt(i);
+                    }
+                }
 
                 break;
 
             case "Botte":
+                if(e.getActionCommand().contains("Inventaire")) {
+
+                    for (int i = 10; i < e.getActionCommand().length(); i++) {
+                        nombre1 += e.getActionCommand().charAt(i);
+                    }
+                }
 
                 break;
 
             case "Bouclier":
+                if(e.getActionCommand().contains("Inventaire")) {
+
+                    for (int i = 10; i < e.getActionCommand().length(); i++) {
+                        nombre1 += e.getActionCommand().charAt(i);
+                    }
+                }
 
                 break;
 
             case "Arme":
+                if(e.getActionCommand().contains("Inventaire")) {
+
+                    for (int i = 10; i < e.getActionCommand().length(); i++) {
+                        nombre1 += e.getActionCommand().charAt(i);
+                    }
+                }
 
                 break;
 
