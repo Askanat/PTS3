8f91dc39ef4328c77750d0f0ddafd4bb9317117e
Raphael-R-R <raphael.ragoomundun.act@openmailbox.org>
2017-03-15 10:16:56 +0100

    Update Direction.java

--- src/model/Direction.java
+++ src/model/Direction.java
@@ -3,6 +3,34 @@ package model;
 /**
  * Created by bastien on 16/11/16.
  */
-public enum Direction {
-    DROITE, GAUCHE
+public class Direction {
+    public final static int DROITE = 0, GAUCHE = 1;
+
+    private int direction;
+
+    public Direction(int dir) {
+    	direction = dir;
+    }
+
+    public void setDirection(int dir) {
+    	direction = dir;
+    }
+
+    public int getDirection() {
+    	return direction;
+    }
+
+    public void inverse() {
+    	if(direction == DROITE)
+    		direction = GAUCHE;
+    	else
+    		direction = DROITE;
+    }
+
+    public void print() {
+    	if(direction == DROITE)
+    		System.err.println("DROITE (" + DROITE + ")");
+    	else
+    		System.err.println("GAUCHE (" + GAUCHE + ")");
+    }
 }
