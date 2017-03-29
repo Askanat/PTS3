37777e4adecb74ae4602ea1080ce8b67ac419100
schliermelvin <miliciiop.77>
2017-03-30 00:50:47 +0200

    Cheveux femme + yeux (yeux non fonctionnels )

--- src/vue/FenetreCreationPersonnage.java
+++ src/vue/FenetreCreationPersonnage.java
@@ -19,7 +19,7 @@ import static vue.Fenetre.*;
 public class FenetreCreationPersonnage extends JPanel {
 
     // nombre de texture différente
-    public static final int NB_YEUX = 1;
+    public static final int NB_YEUX = 2;
     public static final int NB_PEAUX = 5;
     public static final int NB_CHEVEUX = 3;
     public static final int NB_PILOSITE = 3;
